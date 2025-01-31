22efc0b4c2e0338a95c08270d48098a2b54ee1d7
vkhafizov <khafizov.vr@gmail.com>
2025-01-31 14:07:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import Table from './table.js';
 document.addEventListener('DOMContentLoaded', () => {
     const scanButton = document.getElementById('scanButton'); // Кнопка "Сканировать"
     const preview = document.getElementById('preview'); // Элемент <video> для камеры
-    const errorDiv = document.getElementById('error'); // Блок для вывода ошибок
     const productTable = document.getElementById('productTable'); // Таблица
 
     // Инициализация таблицы
@@ -12,22 +11,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Инициализация сканера
     const scanner = new Scanner(preview, (content) => {
-        errorDiv.textContent = ''; // Очищаем сообщение об ошибке
         table.updateTable(content); // Обновляем таблицу данными из QR-кода
     });
 
-    try {
-        scanner.init(); // Инициализируем сканер
-    } catch (error) {
-        errorDiv.textContent = error.message; // Выводим ошибку, если что-то пошло не так
-    }
+    // Инициализация и запуск сканера
+    scanner.init()
+        .then(() => {
+            console.log('Сканер инициализирован.');
+        })
+        .catch((error) => {
+            console.error('Ошибка инициализации сканера:', error);
+        });
 
     // Обработчик нажатия на кнопку "Сканировать"
     scanButton.addEventListener('click', () => {
-        try {
-            scanner.start(); // Запускаем сканирование
-        } catch (error) {
-            errorDiv.textContent = error.message; // Выводим ошибку, если что-то пошло не так
-        }
+        scanner.start(); // Запускаем сканирование
     });
 });
